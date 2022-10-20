e503b750a3852b0abee3e181abd01b20536142dd
Andreas Løhre <andreas.lohre@nomono.co>
2022-10-20 16:36:09 +0200

    Add disconnect and fix scan call

--- station.go
+++ station.go
@@ -1,10 +1,14 @@
 package iwd
 
-import "github.com/godbus/dbus/v5"
+import (
+	"github.com/godbus/dbus/v5"
+)
 
 const (
-	objectStation   = "net.connman.iwd.Station"
-	callStationScan = "net.connman.iwd.Station.Scan"
+	objectStation                 = "net.connman.iwd.Station"
+	callStationScan               = "net.connman.iwd.Station.Scan"
+	callStationDisconnect         = "net.connman.iwd.Station.Disconnect"
+	callStationGetOrderedNetworks = "net.connman.iwd.Station.GetOrderedNetworks"
 )
 
 // Station refers to net.connman.iwd.Station
@@ -17,10 +21,22 @@ type Station struct {
 
 // Scan scans for wireless networks
 func (s *Station) Scan(conn *dbus.Conn) error {
-	obj := conn.Object(objectStation, "")
+	obj := conn.Object(objectIwd, s.Path)
 	call := obj.Call(callStationScan, 0)
 	if call.Err != nil {
 		return call.Err
 	}
+
+	return nil
+}
+
+// Disconnect station
+func (s *Station) Disconnect(conn *dbus.Conn) error {
+	obj := conn.Object(objectIwd, s.Path)
+	call := obj.Call(callStationDisconnect, 0)
+	if call.Err != nil {
+		return call.Err
+	}
+
 	return nil
 }
